1da2a4292f3c532cedad6ee823b282a23b95a297
romaxa <bakirov.important@gmail.com>
2021-10-23 18:29:18 +0700

    Add some functions for enum

--- Src/Function.php
+++ Src/Function.php
@@ -4,6 +4,7 @@ namespace Makhnanov\Telegram81;
 
 use GuzzleHttp\Psr7\Response;
 use GuzzleHttp\Utils;
+use UnitEnum;
 
 function is_set(mixed $value): bool
 {
@@ -13,4 +14,19 @@ function is_set(mixed $value): bool
 function decoded(Response $response): array
 {
     return Utils::jsonDecode($response->getBody()->getContents(), true);
+}
+
+function name(UnitEnum $enum): string
+{
+    return $enum->name;
+}
+
+function value(UnitEnum $enum): string
+{
+    return $enum->name;
+}
+
+function enumToArray(UnitEnum $enum): array
+{
+    return [$enum->name => $enum->value ?? null];
 }

63217264d8a204081595562f6d637c51d3df946d
romaxa <bakirov.important@gmail.com>
2021-10-24 21:40:28 +0700

    InlineKeyboardMarkup::new static function added

--- Src/Api/Type/keyboard/inline/InlineKeyboardMarkup.php
+++ Src/Api/Type/keyboard/inline/InlineKeyboardMarkup.php
@@ -2,9 +2,18 @@
 
 namespace Makhnanov\Telegram81\Api\Type\keyboard\inline;
 
+use JetBrains\PhpStorm\ArrayShape;
 use Makhnanov\Telegram81\Api\Type\SelfFilling;
 
 class InlineKeyboardMarkup extends SelfFilling
 {
-
+    #[ArrayShape(['inline_keyboard' => "array[]"])]
+    public static function new(array $rows): array
+    {
+        return [
+            'inline_keyboard' => [
+                $rows
+            ]
+        ];
+    }
 }

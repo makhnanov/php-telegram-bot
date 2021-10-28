919d556b11634c3633cd99bdfb185d0439c0153e
romaxa <bakirov.important@gmail.com>
2021-10-28 22:49:10 +0700

    Refactor doc

--- Src/Api/Method/Edit/EditMessageTextTrait.php
+++ Src/Api/Method/Edit/EditMessageTextTrait.php
@@ -32,37 +32,36 @@ trait EditMessageTextTrait
      * Use this method to edit text and game messages. On success, if the edited message is not an inline message,
      * the edited Message is returned, otherwise True is returned.
      *
-     * @param string $text New text of the message, 1-4096 characters after entities parsing
+     * @param string|Stringable $text New text of the message, 1-4096 characters after entities parsing
      *
-     * @param ?int|string $chat_id @Optional. Required if inline_message_id is not specified. Unique identifier for
-     *                             the target chat or username of the target channel in the format @channelusername
+     * @param null|int|string|Stringable $chat_id @Optional. Required if inline_message_id is not specified.
+     *                                                       Unique identifier for the target chat or username
+     *                                                       of the target channel in the format @channelusername
      *
-     * @param ?int $message_id @Optional. Required if inline_message_id is not specified. Identifier of the message to edit
+     * @param null|int $message_id @Optional. Required if inline_message_id is not specified.
+     * Identifier of the message to edit
      *
-     * @param ?string $inline_message_id @Optional. Required if chat_id and message_id are not specified.
+     * @param null|string|Stringable $inline_message_id @Optional. Required if chat_id and message_id are not specified.
      *                                   Identifier of the inline message
      *
-     * @param ?ParseMode $parse_mode @Optional. Mode for parsing entities in the message text.
+     * @param null|ParseMode $parse_mode @Optional. Mode for parsing entities in the message text.
      *                               See formatting options for more details.
      *
-     * @param ?MessageEntityCollection $entities @Optional. A JSON-serialized list of special entities that appear
+     * @param null|MessageEntityCollection $entities @Optional. A JSON-serialized list of special entities that appear
      *                                           in message text, which can be specified instead of parse_mode
      *
-     * @param ?Boolean $disable_web_page_preview @Optional. Disables link previews for links in this message
+     * @param null|Boolean $disable_web_page_preview @Optional. Disables link previews for links in this message
      *
      * @param null|array|InlineKeyboardMarkup $reply_markup @Optional. A JSON-serialized object for an inline keyboard.
      *
-     * @param ?array $viaArray
+     * @param null|array $viaArray
      *
-     * @param ?bool $ignoreExceptionIfUnchanged
+     * @param null|bool $ignoreExceptionIfUnchanged
      *
      * @return Message&ResponsiveResultative
      *
-     * @throws BadCodeException
      * @throws UnchangedMessageException
-     *
      * @noinspection PhpUnusedParameterInspection
-     * @noinspection PhpIncompatibleReturnTypeInspection
      */
     public function editMessageText(
         string|Stringable               $text,

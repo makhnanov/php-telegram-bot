83933528f1120c59e4a6b82bf31208125f6303cd
RomaxaCoder <bakirov.important@gmail.com>
2026-01-26 00:01:44 +0500

    Rollback SendMessage.php

---
+++ Src/Method/SendMessage.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Makhnanov\TelegramBot\Method;
+
+use Makhnanov\TelegramBot\Type\Message;
+
+trait SendMessage
+{
+    public function sendMessage(
+        int|string $chatId,
+        string $text,
+        ?string $businessConnectionId = null,
+        ?int $messageThreadId = null,
+        ?string $parseMode = null,
+        ?array $entities = null,
+        ?array $linkPreviewOptions = null,
+        ?bool $disableNotification = null,
+        ?bool $protectContent = null,
+        ?bool $allowPaidBroadcast = null,
+        ?string $messageEffectId = null,
+        ?array $replyParameters = null,
+        ?array $replyMarkup = null,
+    ): Message {
+        $params = [
+            'chat_id' => $chatId,
+            'text' => $text,
+        ];
+
+        if ($businessConnectionId !== null) {
+            $params['business_connection_id'] = $businessConnectionId;
+        }
+        if ($messageThreadId !== null) {
+            $params['message_thread_id'] = $messageThreadId;
+        }
+        if ($parseMode !== null) {
+            $params['parse_mode'] = $parseMode;
+        }
+        if ($entities !== null) {
+            $params['entities'] = $entities;
+        }
+        if ($linkPreviewOptions !== null) {
+            $params['link_preview_options'] = $linkPreviewOptions;
+        }
+        if ($disableNotification !== null) {
+            $params['disable_notification'] = $disableNotification;
+        }
+        if ($protectContent !== null) {
+            $params['protect_content'] = $protectContent;
+        }
+        if ($allowPaidBroadcast !== null) {
+            $params['allow_paid_broadcast'] = $allowPaidBroadcast;
+        }
+        if ($messageEffectId !== null) {
+            $params['message_effect_id'] = $messageEffectId;
+        }
+        if ($replyParameters !== null) {
+            $params['reply_parameters'] = $replyParameters;
+        }
+        if ($replyMarkup !== null) {
+            $params['reply_markup'] = $replyMarkup;
+        }
+
+        $result = $this->api->call('sendMessage', $params);
+
+        return Message::fromArray($result);
+    }
+}

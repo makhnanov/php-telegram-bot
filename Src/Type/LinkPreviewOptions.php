17ae84bc10e89d878ef5bfc5f2c4efe4ef4abc79
RomaxaCoder <bakirov.important@gmail.com>
2026-02-07 10:55:46 +0500

    LinkPreviewOptions

--- Src/Type/LinkPreviewOptions.php
+++ Src/Type/LinkPreviewOptions.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Makhnanov\TelegramBot\Type;
 
-readonly class LinkPreviewOptions
+readonly class LinkPreviewOptions implements \JsonSerializable
 {
     public function __construct(
         public ?bool $isDisabled = null,
@@ -13,6 +13,17 @@ readonly class LinkPreviewOptions
         public ?bool $preferLargeMedia = null,
         public ?bool $showAboveText = null,
     ) {}
+
+    public function jsonSerialize(): array
+    {
+        $result = [];
+        if ($this->isDisabled !== null) $result['is_disabled'] = $this->isDisabled;
+        if ($this->url !== null) $result['url'] = $this->url;
+        if ($this->preferSmallMedia !== null) $result['prefer_small_media'] = $this->preferSmallMedia;
+        if ($this->preferLargeMedia !== null) $result['prefer_large_media'] = $this->preferLargeMedia;
+        if ($this->showAboveText !== null) $result['show_above_text'] = $this->showAboveText;
+        return $result;
+    }
 
     public static function fromArray(array $data): self
     {
